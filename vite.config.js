5d933c33ab71bddaf4675bea980de59eb79164f5
Ace <adamabdallahayln1@gmail.com>
2026-01-20 13:56:13 +0200

    Fixing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { base } from 'framer-motion/client'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/cedarstech-pricelist/',
   plugins: [
     react(),
     tailwindcss(),
