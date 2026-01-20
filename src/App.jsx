cc348aa4a6e465b59a1a1be0b0f42c95eadbc1cd
Ace <adamabdallahayln1@gmail.com>
2026-01-20 14:02:46 +0200

    Last

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Prices from "./pages/Prices";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
 import { useState } from "react";
 
 export default function App() {
-  const [isAuthed, setIsAuthed] = useState(localStorage.getItem("cedars_admin_auth") === "true");
+  const [isAuthed, setIsAuthed] = useState(
+    localStorage.getItem("cedars_admin_auth") === "true"
+  );
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/prices" />} />
-        <Route path="/prices" element={<Prices />} />
-        <Route path="/login" element={<Login setIsAuthed={setIsAuthed} />} />
-        <Route path="/admin" element={isAuthed ? <Admin setIsAuthed={setIsAuthed} /> : <Navigate to="/login" />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Navigate to="/prices" />} />
+      <Route path="/prices" element={<Prices />} />
+      <Route path="/login" element={<Login setIsAuthed={setIsAuthed} />} />
+      <Route
+        path="/admin"
+        element={
+          isAuthed ? <Admin setIsAuthed={setIsAuthed} /> : <Navigate to="/login" />
+        }
+      />
+    </Routes>
   );
 }
