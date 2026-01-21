d814180403b2a0027a6b8308d97d7cccca837ffa
Ace <adamabdallahayln1@gmail.com>
2026-01-21 18:58:23 +0200

    Fix base path + router + Vercel config

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const params = new URLSearchParams(window.location.search);
-const redirect = params.get("redirect");
-
-if (redirect) {
-  window.history.replaceState(null, "", "/streaming" + redirect);
-}
-
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/cedarstech-pricelist">
+  <BrowserRouter basename="/streaming">
     <App />
   </BrowserRouter>
 );
